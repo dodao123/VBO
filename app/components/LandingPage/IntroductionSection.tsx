fe6fbd8a6895d48df84d43b8788b65b907898ce3
dodao123 <dodao9009@gmail.com>
2025-09-14 08:08:47 +0700

    second 2 commit 8

--- app/components/LandingPage/IntroductionSection.tsx
+++ app/components/LandingPage/IntroductionSection.tsx
@@ -47,7 +47,8 @@ const IntroductionSection = () => {
             {/* Description */}
             <p data-aos="fade-up" data-aos-delay="250" className={`${lora.className} text-base md:text-lg text-gray-600 leading-relaxed`}>
   Ngày 22-06-2020 <br />
-  <strong>VƯỜN BIẾT ƠN</strong> Câu Lạc Bộ Đọc Sách Hướng Dương Được thành lập và hoạt động với những thiện pháp lan tỏa lòng biết ơn và văn hóa đọc chạm đến trái tim mọi người.Hướng đến ba giá trị cốt lõi:  <br />
+  <strong>VƯỜN BIẾT ƠN</strong> Câu Lạc Bộ Đọc Sách Hướng Dương được thành lập và hoạt động với những thiện pháp lan tỏa lòng biết ơn và văn hóa đọc chạm đến trái tim mọi người. <br/>
+  Hướng đến ba giá trị cốt lõi:  <br />
  
   + Tu tâm bồi trí tuệ <br />
   + Rèn luyện nghị lực <br />
